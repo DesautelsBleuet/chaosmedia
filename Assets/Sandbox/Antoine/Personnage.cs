0eae3d481fb4b35a7004f6da2c93eb00fb13c4b3
B0bK4t <antoine.guerette@gmail.com>
2021-09-27 15:04:04 -0400

    Ajout limites + input de Rémi

--- Assets/Sandbox/Antoine/Personnage.cs
+++ Assets/Sandbox/Antoine/Personnage.cs
@@ -10,6 +10,13 @@ public class Personnage : MonoBehaviour
     public GameObject joueur; //game object du joueur
     public CharacterController controle; //Contrôleur du personnage
 
+    [Header("Limites X")]
+    public float limiteXBas = -0.27f;
+    public float limiteXGauche = -5f;
+    [Header("Limites Z")]
+    public float limiteZGauche = -0.8f;
+    public float limiteZHaut = 4f;
+
     // https://gamedevbeginner.com/input-in-unity-made-easy-complete-guide-to-the-new-system/#player_input_component_send_messages <-- Source du code ci-dessous
 
     public Vector2 moveVal;
@@ -22,6 +29,19 @@ public class Personnage : MonoBehaviour
 
     void Update()
     {
+        
+        // Debug.Log("x: " + joueur.transform.position.x);
+        // Debug.Log("z: " + joueur.transform.position.z);
+
         controle.transform.Translate(new Vector3(moveVal.x, 0, moveVal.y) * moveSpeed * Time.deltaTime);
+
+        // if (joueur.transform.position.x < limiteXBas 
+        // && joueur.transform.position.x > limiteXGauche
+        // && joueur.transform.position.z > limiteZGauche
+        // && joueur.transform.position.z < limiteZHaut) {
+        //    controle.transform.Translate(new Vector3(moveVal.x, 0, moveVal.y) * moveSpeed * Time.deltaTime);
+        // } else {
+        //     Debug.Log("Out");
+        // }
     }
 }
---
+++ Assets/Sandbox/Remi/Scripts/Mouvement.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Mouvement : MonoBehaviour
+{
+    private CharacterController controller;
+    private PlayerInput playerInput;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    private float playerSpeed = 30.0f;
+    
+    private float gravityValue = -9.81f;
+
+    private void Start()
+    {
+        controller = gameObject.AddComponent<CharacterController>();
+        playerInput = GetComponent<PlayerInput>();
+    }
+
+    void Update()
+    {
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        Vector2 input = playerInput.actions["Move"].ReadValue<Vector2>();
+        Vector3 move = new Vector3(input.x, 0, input.y);
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }
+
+        
+        
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+}
