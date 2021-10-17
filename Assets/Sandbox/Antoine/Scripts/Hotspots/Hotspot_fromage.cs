fcd98713de1108a9b3cfaa79569da261ea5108db
B0bK4t <antoine.guerette@gmail.com>
2021-10-17 15:27:16 -0400

    Positionnement mini jeux

---
+++ Assets/Sandbox/Antoine/Scripts/Hotspots/Hotspot_fromage.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hotspot_fromage : Hotspot
+{
+    private void Start()
+    {
+        objectName = "Mini-jeu de fromage, autre scène?";
+    }
+}
---
+++ Assets/Sandbox/Antoine/Scripts/Hotspots/Hotspot_laitue.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hotspot_laitue : Hotspot
+{
+    private void Start()
+    {
+        objectName = "Mini-jeu de salade, autre scène?";
+    }
+}
