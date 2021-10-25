793ce82c54ce1dffd07b667fa16707e350b2f3fd
Rémi Therrien <therrienremi@gmail.com>
2021-10-25 15:48:04 -0400

    Ajout scene alpha

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,12 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using NativeWebSocket;
+
 
 public class GameManager : MonoBehaviour
 {
+
+    WebSocket websocket;
+
     
+
+    public void startSocket(){
+
+        
+    }
+
+    async void Start(){
+        websocket = new WebSocket("ws://chaosmedia.herokuapp.com/socket/4");
+
+        websocket.OnOpen += () =>
+        {
+            
+            websocket.SendText("dasdad­­");
+        };
+        websocket.OnMessage += (bytes) =>
+    {
+        var message = System.Text.Encoding.UTF8.GetString(bytes);
+        Debug.Log(message);
+        // dynamic json = JsonConvert.DeserializeObject(message);
+
+        // string code = (string)json.SelectToken("code");
+        // json data = (json)json.SelectToken("data");
+
+        // if(code == "__joined__") {
+        //     Debug.Log("Vous avez rejoint la partie.");
+        // }
+
+    };
+    }
     public void DebutPartie()
     {
         SceneManager.LoadScene("scene_alpha"); //charger la scène alpha
+        
     }
 }
