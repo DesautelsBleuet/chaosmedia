6b18d585ee4f9cde2a26e893b9ceb9131f050ee7
Antoine Guérette <antoine.guerette@gmail.com>
2021-10-28 16:41:29 -0400

    Fin du script pour l'assamblage

--- Assets/Sandbox/Antoine/Scripts/TestArray.cs
+++ Assets/Sandbox/Antoine/Scripts/TestArray.cs
@@ -5,11 +5,10 @@ using UnityEngine;
 
 public class TestArray : MonoBehaviour
 {
-    string[] contentArray = new string[] {"fromage", "pain", "viande", "laitue", "tomate"};
+    string[] contentArray = new string[] {};
     Dictionary<string, string[]> repasArray = new Dictionary<string, string[]>();
 
     //----------Repas
-    //Brochette
     string[] brochetteArray = new string[] {"viande", "laitue", "tomate"};
     string[] burgerArray = new string[] {"fromage", "pain", "viande", "laitue", "tomate"};
     string[] croqueMonsieurArray = new string[] {"fromage", "pain", "viande"};
@@ -30,11 +29,53 @@ public class TestArray : MonoBehaviour
        for (int i = 0; i < repasArray.Count; i++)
        {
             string repas = repasArray.ElementAt(i).Key;
-            Debug.Log(repas);
-            
-        }
-        
-        
-    }
+            string[] ingredientsNeeded = repasArray.ElementAt(i).Value;
+            List<string> allNeeded = new List<string>();
+            string done = "false";
 
+            foreach (var ingredient in ingredientsNeeded)
+            {
+                allNeeded.Add(ingredient);
+            }   
+            
+            if (contentArray.Count() > 0) {
+                foreach (var ingredient in contentArray)
+                    {
+                    if (!ingredientsNeeded.Contains(ingredient)) {
+                            done = "false";
+                            break;
+                        } else {
+                            done = "half";
+                            allNeeded.Remove(ingredient);
+                            if (allNeeded.Count == 0) {
+                                done = "true";
+                            }
+                        }
+                    }
+            } else {
+            done = "half";
+            }
+
+        if (done == "false")
+            {
+                Debug.Log(repas);
+                Debug.Log("ne peut pas être fait");
+            } else if (done == "half") {
+                
+                Debug.Log(repas);
+                Debug.Log("n'est pas terminé");
+                Debug.Log("il manque:");
+                foreach (var item in allNeeded)
+                {
+                    Debug.Log(item);
+                }
+            } else if (done == "true") {
+                Debug.Log(repas);
+                Debug.Log("est terminé");
+            }
+
+            Debug.Log("---------Prochain repas-----------");
+        }
+
+    }
 }
