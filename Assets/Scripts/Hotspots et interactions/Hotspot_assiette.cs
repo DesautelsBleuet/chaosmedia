d0b28421da1cf0b6d038f0d755331012e978648c
Antoine Guérette <antoine.guerette@gmail.com>
2021-11-13 21:36:14 -0500

    Enlever les éléments de l'assiette

--- Assets/Scripts/Hotspots et interactions/Hotspot_assiette.cs
+++ Assets/Scripts/Hotspots et interactions/Hotspot_assiette.cs
@@ -10,8 +10,11 @@ public class Hotspot_assiette : MonoBehaviour
     public GameObject plate;
     private bool canInteract = true;
     private int nbIngredients = 0;
-    private List<Transform> ingredients = new List<Transform>();
-    private Transform ingredientTransform;
+    private List<GameObject> ingredients = new List<GameObject>();
+    private List<Vector3> originals = new List<Vector3>();
+    private GameObject ingredientClone;
+    private GameObject ingredientCarry;
+    public Vector3 offset;
 
     void Awake() {
         plate = GameObject.Find("Plate");
@@ -23,7 +26,6 @@ public class Hotspot_assiette : MonoBehaviour
             canInteract = false;
             if (other.GetComponent<Objets>().isCarrying) {
                 ingredient = other.GetComponent<Objets>().ingredient;
-                Debug.Log(ingredient.transform.localScale);
                 ajoutIngredient(ingredient);
                 other.GetComponent<Objets>().isCarrying = false;
                 Destroy(ingredient);
@@ -39,16 +41,28 @@ public class Hotspot_assiette : MonoBehaviour
 
     void ajoutIngredient(GameObject ingredient) {
         GameManager.SendMessage("ajoutIngredient", player.GetComponent<Objets>().ingredient.tag);
-        ingredientTransform = Instantiate(ingredient.transform, new Vector3(0, 0, 0), Quaternion.identity, plate.transform);
-        ingredientTransform.localPosition = new Vector3(0,0, 0.017f*(nbIngredients+1));
-        ingredientTransform.localScale = new Vector3(0.5f,0.5f,2);
-        ingredientTransform.localRotation = Quaternion.Euler(new Vector3(0, 0, 0));
+        var originalScale = new Vector3(ingredient.transform.localScale.x, ingredient.transform.localScale.y, ingredient.transform.localScale.z);
+        ingredientClone = Instantiate(ingredient, new Vector3(0, 0, 0), Quaternion.identity, plate.transform);
+        ingredientClone.transform.localPosition = new Vector3(0,0, 0.017f*(nbIngredients+1));
+        ingredientClone.transform.localScale = new Vector3(0.5f,0.5f,2);
+        ingredientClone.transform.localRotation = Quaternion.Euler(new Vector3(0, 0, 0));
         nbIngredients ++;
-        ingredients.Add(ingredientTransform);
+        ingredients.Add(ingredientClone);
+        originals.Add(originalScale);
     }
 
     void enleverIngredient() {
         if (nbIngredients > 0) {
+            var lastElement = ingredients.Count - 1;
+            var o = originals[lastElement];
+            ingredientCarry = Instantiate(ingredients[lastElement], new Vector3(0, 0, 0), Quaternion.identity);
+            ingredientCarry.transform.localScale = o;
+            player.GetComponent<Objets>().isCarrying = true;
+            player.GetComponent<Objets>().ingredient = ingredientCarry;
+            player.GetComponent<Objets>().offset = offset;
+            player.GetComponent<Objets>().currentParent = this.gameObject;
+            Destroy(ingredients[lastElement]);
+            ingredients.RemoveAt(lastElement);
             nbIngredients--;
             GameManager.SendMessage("enleverIngredient");
         }
